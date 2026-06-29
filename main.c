0aeef3fcea629d63482a11b511cc3fa47a442dee
lucasrodrigueslrm15-spec <lucasrodrigueslrm15@gmail.com>
2026-06-29 08:36:37 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+    int r = 1;
+    while (r){
+        char command[1000];
+
+        printf("&: ");
+        fgets(command, sizeof(command), stdin);
+        
+        int res = system(command);
+        printf(res == -1 ? "HOHOHO, your doomed.\n" : "\n");
+    if (strcmp(command, "exit\n") == 0){
+            r=0;
+        }
+    }
+    return 0;
+}
